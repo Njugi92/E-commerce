32a06f6aa85a4a5015523df702b72c451baf77bb
Njugi92 <kenkaruma.kk@gmail.com>
2024-03-21 05:52:22 +0300

    created a login form

--- e-commers-app/src/Pages/LoginSignup.jsx
+++ e-commers-app/src/Pages/LoginSignup.jsx
@@ -3,6 +3,21 @@ import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div className='loginsignup'></div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" id='' />
+          <p>By continuing, I agree to the terms of use and privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
